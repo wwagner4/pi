bf3e7dcffcea8a120674dd308e98c53dcd607eb9
wwagner <wolfgang.wagner@itsv.at>
2021-08-10 19:00:50 +0200

    added hilbert class

---
+++ src/main/scala/pi/Hilbert.scala
@@ -0,0 +1,11 @@
+package pi
+
+import pi.Main.Line
+
+class Hilbert {
+
+  def draw(level: Int): List[Line] = {
+    ???
+  }
+
+}
--- src/main/scala/pi/Main.scala
+++ src/main/scala/pi/Main.scala
@@ -2,6 +2,10 @@ package pi
 
 object Main {
 
+  case class Point(x: Double, y:Double)
+
+  case class Line(start: Point, end:Point)
+
   def main(args: Array[String]): Unit ={
     println("Hallo")
   }
