305a3af10747b3e44643cb81bf1710b4e79996e3
wwagner4 <wwagner4@gmail.com>
2021-08-13 22:15:18 +0200

    up and down OK

--- src/main/scala/pi/Hilbert.scala
+++ src/main/scala/pi/Hilbert.scala
@@ -32,7 +32,7 @@ object Hilbert {
       if level < 0 then (Seq.empty[Line], Seq.empty[Line], Seq.empty[Line])
       else {
         val s1 = side / 4.0
-        val s3 = side * 3.0
+        val s3 = s1 * 3.0
         definition match {
           case HilbertDefinition(Orientation.Up, Direction.Clockwise) => 
             val a = Seq(Line(co1, origin.add(s1, s1), origin.add(s1, s3)))
@@ -40,9 +40,9 @@ object Hilbert {
             val c = Seq(Line(co3, origin.add(s3, s3), origin.add(s3, s1)))
             (a, b, c)
           case HilbertDefinition(Orientation.Down, Direction.Clockwise) => 
-            val a = Seq(Line(co1, origin.add(-s1, -s1), origin.add(-s1, -s3)))
-            val b = Seq(Line(co2, origin.add(-s1, -s3), origin.add(-s3, -s3)))
-            val c = Seq(Line(co3, origin.add(- s3, -s3), origin.add(-s3, -s1)))
+            val a = Seq(Line(co1, origin.add(s1, -s1), origin.add(s1, -s3)))
+            val b = Seq(Line(co2, origin.add(s1, -s3), origin.add(s3, -s3)))
+            val c = Seq(Line(co3, origin.add(s3, -s3), origin.add(s3, -s1)))
             (a, b, c)
           case HilbertDefinition(Orientation.Right, Direction.Clockwise) => 
             ???
@@ -91,10 +91,17 @@ object Hilbert {
     }
 
     val side = math.min(canvas.width, canvas.height).toDouble
-    val origin = Point(0, canvas.height)
+
+/*
+    val origin = Point(0, 0)
     val startDefinition = HilbertDefinition(Orientation.Up, Direction.Clockwise)
+*/
+    val origin = Point(0, side)
+    val startDefinition = HilbertDefinition(Orientation.Down, Direction.Clockwise)
     val re = drawRecursive(level, origin, side, startDefinition)
+    println(startDefinition)
     println(origin)
+    println(side)
     printPoli(re)
     re
   }
